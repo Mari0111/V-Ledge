50f792841f717a4e24c6f27740a48eb9bfe2f1ae
Mari0111 <nyalentine@gmail.com>
2022-05-05 00:49:31 +0800

    schedule and icon replaced

--- components/VNavBar/index.js
+++ components/VNavBar/index.js
@@ -9,15 +9,13 @@ import InputBase from "@mui/material/InputBase";
 import Badge from "@mui/material/Badge";
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
-import SearchIcon from "@mui/icons-material/Search";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import MailIcon from "@mui/icons-material/Mail";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import MoreIcon from "@mui/icons-material/MoreVert";
-import FavoriteBorder from "@mui/icons-material/FavoriteBorder";
-import ShoppingCart from "@mui/icons-material/ShoppingCart";
 import { Link } from "@mui/material";
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import SchoolIcon from '@mui/icons-material/School';
 
 const Search = styled("div")(({ theme }) => ({
   position: "relative",
@@ -164,7 +162,7 @@ export default function VNavBar() {
       <Box sx={{ flexGrow: 1 }}>
         <AppBar position="sticky" style={{ background: "#ffa726" }}>
           <Toolbar>
-            <IconButton
+            {/* <IconButton
               size="large"
               edge="start"
               color="inherit"
@@ -172,7 +170,7 @@ export default function VNavBar() {
               sx={{ mr: 2 }}
             >
               <MenuIcon />
-            </IconButton>
+            </IconButton> */}
             <Typography
               variant="h6"
               noWrap
@@ -181,7 +179,7 @@ export default function VNavBar() {
             >
               V-Legde
             </Typography>
-            <Search>
+            {/* <Search>
               <SearchIconWrapper>
                 <SearchIcon />
               </SearchIconWrapper>
@@ -189,25 +187,34 @@ export default function VNavBar() {
                 placeholder="Search…"
                 inputProps={{ "aria-label": "search" }}
               />
-            </Search>
+            </Search> */}
             <Box sx={{ flexGrow: 1 }} />
             <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            <IconButton
+                size="large"
+                aria-label="show 4 new mails"
+                color="inherit"
+              >
+   
+                  <SchoolIcon />
+                
+              </IconButton>
               <IconButton
                 size="large"
                 aria-label="show 4 new mails"
                 color="inherit"
               >
-                <Badge badgeContent={4} color="error">
-                  <FavoriteBorder />
-                </Badge>
+    
+                  <EventNoteIcon />
+    
               </IconButton>
               <IconButton
                 size="large"
                 aria-label="show 17 new notifications"
                 color="inherit"
               >
-                <Badge badgeContent={17} color="error">
-                  <ShoppingCart />
+                <Badge badgeContent={4} color="error">
+                  <NotificationsIcon  />
                 </Badge>
               </IconButton>
               <IconButton
